35591cc840f8e6f4897e9e5553a2a823cc332e3a
Irakli <irakli.ghachava@gmail.com>
2021-07-20 01:07:04 +0400

    graphs

---
+++ graph.js
@@ -0,0 +1,48 @@
+class Graph {
+	constructor() {
+		this.adjacencyList = {};
+	}
+	validate(nodeOne, nodeTwo) {
+		if (!nodeOne || !nodeTwo) throw new Error("Unsupported inputs.");
+		if (!this.adjacencyList[nodeOne] || !this.adjacencyList[nodeTwo])
+			throw new Error("One of the provided nodes does not exist.");
+	}
+	addNode(node) {
+		if (!this.adjacencyList[node]) this.adjacencyList[node] = [];
+		return this.adjacencyList;
+	}
+	removeNode(node) {
+		if (!this.adjacencyList[node]) throw new Error("Node does not exist.");
+		this.adjacencyList[node].forEach((i) => {
+			this.removeLink(node, i);
+		});
+		// OPTIONAL
+		delete this.adjacencyList[node];
+	}
+	addLink(nodeOne, nodeTwo) {
+		this.validate(nodeOne, nodeTwo);
+		this.adjacencyList[nodeOne].push(nodeTwo);
+		this.adjacencyList[nodeTwo].push(nodeOne);
+	}
+	removeLink(nodeOne, nodeTwo) {
+		this.validate(nodeOne, nodeTwo);
+		this.adjacencyList[nodeOne] = this.adjacencyList[nodeOne].filter(
+			(i) => i !== nodeTwo
+		);
+		this.adjacencyList[nodeTwo] = this.adjacencyList[nodeTwo].filter(
+			(i) => i !== nodeOne
+		);
+	}
+}
+
+const graph = new Graph();
+graph.addNode("Ika");
+graph.addNode("Teo");
+graph.addNode("Nini");
+graph.addNode("Gio");
+graph.addLink("Ika", "Nini");
+graph.addLink("Gio", "Ika");
+graph.addLink("Gio", "Nini");
+graph.addLink("Ika", "Teo");
+graph.removeNode("Ika");
+console.log(graph);
